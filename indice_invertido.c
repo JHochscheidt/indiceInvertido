4995056626240712c40cd02ea66c850ae33c6503
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-20 16:37:47 -0300

    funcoes para leitura de um arquivo

--- indice_invertido.c
+++ indice_invertido.c
@@ -1,1 +1,20 @@
 #include "indice_invertido.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ler_arquivo(FILE *arq){
+	char c;
+	while(c != EOF){
+		c = getc(arq);
+		printf("%c", c);
+	}
+}
+
+FILE* gerar_texto(char* nome_arquivo, char* diretorio){
+	char* caminho = strcat(diretorio,nome_arquivo);
+	FILE *arq;
+	arq = fopen(caminho,"r");
+	return arq;
+}
+
