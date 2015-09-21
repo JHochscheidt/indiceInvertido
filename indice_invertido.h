6aaadd655788aa4a245893e7e4f8825cf34e9588
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-21 00:20:39 -0300

    criada funcao menu() apenas para imprimir o menu na tela. Criado funcao **verifica_arquivos_entrada(...) que faz a verificacao dos arquivos passados por parametro na linha de comando

--- indice_invertido.h
+++ indice_invertido.h
@@ -3,11 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TRUE 1
+#define FALSE 0
 
 
+void menu();
 
+// funcao que guarda o nome dos arquivos a serem processados
+char* nomeArquivo();
 
-
+// funcao que verifica o arquivo de entrada e se os arquivos existem
+char **verifica_arquivos_entrada(int argc, char **argv);
 
 void ler_arquivo(FILE *arq);
 
