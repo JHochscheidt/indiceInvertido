83a14ca91ee12a636b830ee4662b4762ae585c39
Jackson Hochscheidt <jackson94h@gmail.com>
2015-09-20 22:48:01 -0300

    funcao que ler arquivo e gera texto. OBS: gera texto  nao esta implementada ainda

--- indice_invertido.h
+++ indice_invertido.h
@@ -1,8 +1,17 @@
 #ifndef _INDICE_INVERTIDO_H
-
+#define INDICE_INVERTIDO_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+
+
+
+
+
+void ler_arquivo(FILE *arq);
+
+FILE* gerar_texto(char* nome_arquivo, char* diretorio);
 
 
 
